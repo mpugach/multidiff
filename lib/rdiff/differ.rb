321d8e0578c0c6413a50e2fa8a963c49a0912558
Maksym Pugach <pugach.m@gmail.com>
2016-05-15 11:02:33 +0300

    implement differ

--- lib/rdiff/differ.rb
+++ lib/rdiff/differ.rb
@@ -1,17 +1,23 @@
+require 'diff/lcs'
+
 module Rdiff
   class Differ
-    def self.diff
-      output = [
-        '1 * Some|Another',
-        '2 - Simple',
-        '3 Text',
-        '4 File',
-        '5 + With',
-        '6 + Additional',
-        '7 + Lines'
-      ].join("\n")
+    OVERRIDE_ACTION = Hash.new { |hash, key| hash[key] = key }.merge('!' => '*', '=' => ' ')
 
-      puts output
+    def self.diff(files)
+      base = files.shift
+
+      files.map do |file|
+        Diff::LCS.sdiff(base, file).each_with_index.map do |change, line|
+          text = if change.action == '!'
+                   "#{change.old_element}|#{change.new_element}"
+                 else
+                   change.old_element || change.new_element
+                 end
+
+          "#{line.next}\t#{OVERRIDE_ACTION[change.action]}\t#{text}"
+        end
+      end
     end
   end
 end
--- lib/rdiff/runner.rb
+++ lib/rdiff/runner.rb
@@ -1,7 +1,23 @@
 module Rdiff
   class Runner
-    def self.invoke(_args)
-      puts Rdiff::Differ.diff
+    def self.invoke(args)
+      data = read_files(args)
+
+      abort 'Nothing to compare' if data.count < 2
+
+      puts Rdiff::Differ.diff(data).map { |diff| diff.join("\n") }.join("\n\n")
+    end
+
+    class << self
+      private
+
+      def read_files(paths)
+        paths.each_with_object([]) do |path, memo|
+          pathname = Pathname.new(path)
+
+          memo << pathname.read.split("\n") if pathname.file?
+        end
+      end
     end
   end
 end
